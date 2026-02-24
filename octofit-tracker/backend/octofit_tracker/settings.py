5407a59efb974738a0d89820a82647beb1a65460
Sash Kavalaparambil <sash_george@hotmail.com>
2026-02-24 18:40:09 +0000

    Update settings and add API root view for dynamic URL generation based on Codespace environment

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,8 +26,12 @@ SECRET_KEY = 'django-insecure-uoz(t*j-mgv5(g4dyoc+4wc!$0+$5i$we%v_jlo#%&(*hu-clq
 DEBUG = True
 
 
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
+# Allow localhost and Codespace URL
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
